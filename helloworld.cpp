64978173e69dbfd49b7a3aada140bb05ae31159c
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-01-26 08:53:43 +0800

    ada

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,9 +5,13 @@ int main (){
 
     srand(time (NULL));
 
-    int num = rand();
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
 
-    std::cout <<num;
+    std::cout <<num1<<'\n';
+    std::cout <<num2<<'\n';
+    std::cout <<num3<<'\n';
     
     return 0;
 }
